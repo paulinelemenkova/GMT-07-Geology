c64932f7cbdfcabea59ab30635596e7e66809b19
Pauline Lemenkova <pauline.lemenkova@gmail.com>
2020-07-11 18:39:30 +0300

    Add files via upload

--- GMT-01-JM-geol-HT.sh
+++ GMT-01-JM-geol-HT.sh
@@ -25,18 +25,18 @@ gdalinfo ht_relief.nc -stats
 #  Minimum=-5119.000, Maximum=3505.000
 # Make color palette
 # makecpt --help
-gmt makecpt -Cgeo.cpt -V -T-5119/3505 > myocean.cpt
+gmt makecpt -Cglobe.cpt -V -T-5119/3505 > myocean.cpt
 
 # Generate a file
-ps=Bathymetry_HT.ps
+ps=Geol_HT.ps
 # Make raster image
 gmt grdimage ht_relief.nc -Cmyocean.cpt -R19/37/30.5/41.5 -JM6i -P -I+a15+ne0.75 -Xc -K > $ps
 
 # Add grid
 gmt psbasemap -R -J \
-    -Bpx4f1a1 -Bpyg4f1a1 -Bsxg2 -Bsyg2 \
+    -Bpx4f1a2 -Bpyg4f1a2 -Bsxg2 -Bsyg2 \
     --MAP_TITLE_OFFSET=0.8c \
-    -B+t"Geologic map of the Aegean Sea region" -O -K >> $ps
+    -B+t"Geologic map of the Eastern Mediterranean Sea region" -O -K >> $ps
     
 # Add shorelines
 gmt grdcontour ht_relief.nc -R -J -C1000 -W0.1p -O -K >> $ps
@@ -56,7 +56,7 @@ gmt pscoast -R -J -P \
 gmt psscale -Dg16.0/30.5+w11.4c/0.4c+v+o0.3/0i+ml -R -J -Cmyocean.cpt \
     --FONT_LABEL=6p,Helvetica,dimgray \
     --FONT_ANNOT_PRIMARY=6p,Helvetica,black \
-    -Baf+l"Color scale: geo global bathymetry/topography relief [R=-5119/3505, H=0, C=RGB]" \
+    -Baf+l"Colors for global bathymetry/topography relief [R=-5119/3505, H=0, C=RGB]" \
     -I0.2 -By+lm -O -K >> $ps
     
 gmt psxy -R -J trench.gmt -Sf1.5c/0.2c+l+t -Wthick,yellow -Gyellow -O -K >> $ps
@@ -75,14 +75,95 @@ gmt psmeca CMT.txt -R -J -Sd0.5/2/u -Gred -L0.1p -Fa/5p/it \
     -Fepurple -Fgmagenta -Ft -F+f8p,Times-Roman,yellow+jLB \
     -W0.1p -Fz -Ewhite -O -K >> $ps
 
+# Texts
+gmt pstext -R -J -N -O -K \
+-F+jTL+f10p,Helvetica,black+jLB -Gwhite@20 >> $ps << EOF
+21.0 31.5 AFRICAN PLATE
+20.5 40.5 EURASIAN PLATE
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f10p,Helvetica,black+jLB -Gwhite@40 >> $ps << EOF
+31.0 39.2 ANATOLIAN PLATE
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f8p,Helvetica,black+jLB+a-290 -Gwhite@20 >> $ps << EOF
+35.0 31.5 ISRAEL
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f10p,Helvetica,yellow+jLB >> $ps << EOF
+22.2 33.5 M  E  D  I  T  E  R  R  A  N  E  A  N   S  E  A
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f10p,Helvetica,blue+jLB -Gwhite@30 >> $ps << EOF
+24.1 39.0 A E G E A N
+24.7 38.5 PLATE
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f7p,Helvetica,black+jLB -Gwhite@30 >> $ps << EOF
+27.0 40.6 Sea of Marmara
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f7p,Helvetica,black+jLB -Gwhite@30 >> $ps << EOF
+24.5 35.1 Crete
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f7p,Helvetica,black+jLB -Gwhite@20 >> $ps << EOF
+21.4 37.8 Peloponnese
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f8p,Helvetica,black+jLB+a-335 -Gwhite@30 >> $ps << EOF
+32.8 34.9 Cyprus
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f9p,Helvetica,white+jLB+a-45 >> $ps << EOF
+21.0 36.5 H e l l e n i c  T r e n c h
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f9p,Helvetica,white+jLB+a-338 >> $ps << EOF
+25.0 33.9 Pliny Trench
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f9p,Helvetica,white+jLB+a-333 >> $ps << EOF
+26.2 33.9 Strabo Trench
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f9p,Helvetica,white+jLB+a-8 >> $ps << EOF
+31.5 34.3 Cyprus
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f9p,Helvetica,white+jLB+a-350 >> $ps << EOF
+32.8 34.2 Trench
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f7p,Helvetica,black+jLB+a-30 -Gwhite@30 >> $ps << EOF
+23.5 38.8 Evia
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f9p,Helvetica,blue+jLB -Gwhite@30 >> $ps << EOF
+29.2 41.25 Black Sea
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f7p,Helvetica,black+jLB -Gwhite@30 >> $ps << EOF
+25.5 36.5 Santorini
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f7p,Helvetica,black+jLB -Gwhite@30 >> $ps << EOF
+27.5 36.5 Rhodes
+EOF
+gmt pstext -R -J -N -O -K \
+-F+jTL+f9p,Helvetica,blue+jLB -Gwhite@30 >> $ps << EOF
+24.2 36.1 Cretan
+24.4 35.6 Sea
+EOF
+
 # Add GMT logo
 gmt logo -Dx6.2/-2.0+o0.1i/0.1i+w2c -O -K >> $ps
 
 # Add subtitle
 gmt pstext -R0/10/0/15 -JX10/10 -X0.5c -Y5.7c -N -O \
     -F+f10p,Palatino-Roman,black+jLB >> $ps << EOF
-2.0 9.7 Topographic grid: GEBCO 15 arc sec resolution global terrain model
+1.8 9.7 Topographic grid: GEBCO 15 arc sec resolution global terrain model
 EOF
 
 # Convert to image file using GhostScript
-gmt psconvert Bathymetry_HT.ps -A0.5c -E720 -Tj -Z
+gmt psconvert Geol_HT.ps -A0.5c -E720 -Tj -Z
